0266dc7e7dfc1c5a6c6129aafd4e16e527ba3faf
jonkolar <98371107+jonkolar@users.noreply.github.com>
2023-08-27 16:33:27 -0400

    Added missing months into admin signups chart

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,33 +1,28 @@
 import { useEffect } from "react";
+import dynamic from "next/dynamic";
 
 import { useSession, signIn, signOut } from "next-auth/react"
 import moment from "moment";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]";
 
 import { db } from "@/utils/db-server"
 
+const AdminSignupsChart = dynamic(
+    () => import('../../components/AdminSignupsChart'),
+    { ssr: false, loading: () => <p>Loading...</p> }
+  )
 import { Box } from "@mui/material";
 
 export default function Admin({ user, data }) {
-
-    console.log(data.signupsByMonth)
-
     return (
         <>
             <h1>Admin</h1>
 
             <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                 <h3>Signups</h3>
-                <LineChart width={600} height={300} data={data.signupsByMonth} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                    <Line type="monotone" dataKey="signups" stroke="#8884d8" />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <XAxis dataKey="label" />
-                    <YAxis allowDecimals={false} />
-                    <Tooltip />
-                </LineChart>
+                <AdminSignupsChart data={data.signupData} />
             </Box>
         </>
     )
@@ -42,11 +37,13 @@ export async function getServerSideProps(context) {
 
     let data = {}
 
-    // user count (can you downcast in prisma SQL???)
+    // SIGNUP DATA
+
+    // Query signups by month data from database then downcast (because mysql returns bigint which is not serializable)
     const userCreatedQueryResult = await db.$queryRaw`SELECT MONTH(created) AS created_month, YEAR(created) as created_year, COUNT(*) AS count 
                                       from User 
                                       GROUP BY created_year, created_month`
-    const signupsByMonth = userCreatedQueryResult.map(d => {
+    let signupsByMonth = userCreatedQueryResult.map(d => {
         const year = parseInt(d.created_year)
         const month = moment().month(parseInt(d.created_month - 1)).format("MMMM")
         return { year: year, 
@@ -55,7 +52,27 @@ export async function getServerSideProps(context) {
                  label: month + " " + year }
     })
 
-    data['signupsByMonth'] = signupsByMonth
+    // Fill in missing months of the year
+    let distinctYears = []
+    signupsByMonth.forEach(su => !(su.year in distinctYears) && distinctYears.push(su.year))
+    let signupData = []
+    distinctYears.forEach(year => {
+        for (let i = 0; i < 12; i++) {
+            const currentMonth = moment().month(i).format("MMMM")
+            const signupMonth = signupsByMonth.find(su => su.month == currentMonth)
+            if (!signupMonth) {
+                signupData.push({
+                    year: year,
+                    month: currentMonth,
+                    signups: 0,
+                    label: currentMonth + " " + year
+                })
+            } else {
+                signupData.push(signupMonth)
+            }
+        }
+    })
+    data['signupData'] = signupData
 
     return {
         props: { user: session.user, data: data },
