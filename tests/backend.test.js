38a1b3ce541cb5af34f7df4db3ef173ab0942ec3
jonkolar <98371107+jonkolar@users.noreply.github.com>
2023-09-03 17:14:08 -0400

    Added various tests for team, teamChallenge, and teamMatch

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -1,6 +1,6 @@
 import { createMockContext } from "./test-db-context";
 
-import { createTeam } from "@/utils/db/team-service";
+import { createTeam, deleteTeam, createTeamMatch, deleteTeamMatchChallenge } from "@/utils/services/team-service";
 
 let mockCtx
 let ctx
@@ -10,6 +10,7 @@ beforeEach(() => {
     ctx = mockCtx
 })
 
+// TEAM
 test('create a team', async () => {
     // prepare
     const user = {
@@ -30,5 +31,54 @@ test('create a team', async () => {
         name: 'test team',
         gameId: 1,
         description: 'test description'
+    })
+})
+
+test('delete a team', async () => {
+    // prepare
+    const team = {
+        id: 1
+    }
+
+    // mock
+    mockCtx.prisma.team.delete.mockResolvedValue(team)
+
+    // compare
+    await expect(deleteTeam(ctx.prisma, team.id)).resolves.toEqual({
+        id: 1
+    })
+})
+
+test('create a team match', async () => {
+    // prepare
+    const match = {
+        team1Id: 1,
+        team2Id: 2,
+        date: Date.now()
+    }
+
+    // mock
+    mockCtx.prisma.match.create.mockResolvedValue(match)
+
+    // compare
+    await expect(createTeamMatch(ctx.prisma, match.team1Id, match.team2Id)).resolves.toEqual({
+        team1Id: 1,
+        team2Id: 2,
+        date: match.date
+    })
+})
+
+test('delete a team match challenge', async () => {
+    // prepare
+    const matchChallenge = {
+        id: 1
+    }
+
+    // mock
+    mockCtx.prisma.matchChallenge.delete.mockResolvedValue(matchChallenge)
+
+    // compare
+    await expect(deleteTeamMatchChallenge(ctx.prisma, matchChallenge.id)).resolves.toEqual({
+        id: 1
     })
 })
